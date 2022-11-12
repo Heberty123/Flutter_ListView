9e89de06d8c70b6b38e7a023de16b354c7788f80
Heberty Guelfi do Prado <hebertyprado22005@gmail.com>
2022-11-12 06:33:58 -0300

    list view was added

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,43 +1,96 @@
 import 'package:flutter/material.dart';
+import '../models/todo.dart';
+import '../widgets/todo_list_item.dart';
 
-class TodoListPage extends StatelessWidget {
+class TodoListPage extends StatefulWidget {
   TodoListPage({Key? key}) : super(key: key);
 
   @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+
+  final TextEditingController todoController = TextEditingController();
+
+  List<Todo> todos = [];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Row(
-            children: const [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Adicione uma tarefa',
-                    hintText: 'Ex. Estudar Flutter',
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: todoController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Adicione uma tarefa',
+                          hintText: 'Ex. Estudar Flutter',
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 8,),
+                    ElevatedButton(
+                      onPressed: (){
+                        String text = todoController.text;
+                        setState(() {
+                          Todo newTodo = new
+                          todos.add(text.);
+                          todoController.clear();
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xff00d7f3),
+                      ),
+                      child: Icon(
+                        Icons.add,
+                        size: 30,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16,),
+                Flexible(
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      for(String todo in todos)
+                        TodoListItem(
+                          title: todo,
+                        ),
+                    ],
                   ),
                 ),
-              ),
-              SizedBox(width: 8,),
-              ElevatedButton(
-                  onPressed: null,
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.teal,
-                    padding: EdgeInsets.all(14),
-                  ),
-                  child: Icon(
-                    Icons.add,
-                    size: 30,
-                  ),
-              ),
-            ],
+                SizedBox(height: 16,),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        'Você possui ${todos.length} tarefas pendentes',
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xff00d7f3),
+                      ),
+                        child: Text('Limpar tudo'),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
-
-
 }
